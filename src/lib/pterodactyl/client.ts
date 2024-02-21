e49cbe73d7aaccee67901b527e0227c8496bfe63
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2024-02-21 16:11:07 +0100

    Pterodactyl error handler

--- src/lib/pterodactyl/client.ts
+++ src/lib/pterodactyl/client.ts
@@ -1,16 +1,17 @@
 import { env } from '$env/dynamic/private';
+import { logger } from '$lib/logger';
 import { Application, Client } from 'jspteroapi';
 
 export const pteroAdmin = new Application(
 	env.PTERODACTYL_URL ?? '',
 	env.PTERODACTYL_APP_API_KEY ?? '',
-	undefined,
+	(error) => logger.error(error),
 	true,
 );
 
 export const pteroUser = new Client(
 	env.PTERODACTYL_URL ?? '',
 	env.PTERODACTYL_USER_API_KEY ?? '',
-	undefined,
+	(error) => logger.error(error),
 	true,
 );
